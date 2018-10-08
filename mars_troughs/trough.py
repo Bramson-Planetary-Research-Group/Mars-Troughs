6ccbb40d59de720f840e91d118d4dd2201024203
Tom McClintock <tmcclintock89@gmail.com>
2018-10-07 22:38:01 -0400

    Working on the trough object.

--- mars_troughs/trough.py
+++ mars_troughs/trough.py
@@ -1,8 +1,12 @@
 """This has the trough object with all the necessary functions.
 """
+import numpy as np
 import cffi
 import glob
 import os
+#Both of these are temporary until interpolators are implemented in C
+from scipy.interpolate import InterpolatedUnivariateSpline as IUS
+from scipy.interpolate import interp2d
 
 mars_troughs_dir = os.path.dirname(__file__)
 include_dir = os.path.join(mars_troughs_dir,'include')
@@ -32,7 +36,19 @@ class Trough(object):
         """
         self.params = params
         self.model_number = model_number
-        #Here, possibly load in auxiliary data
+        #Load in supporting data
+        insolation, ins_times = np.loadtxt("Insolation.txt", skiprows=1).T
+        ins_times = -ins_times #positive times are now in the past
+        self.insolation = insolation
+        self.ins_times  = ins_times
+        #Create the look up table for retreats
+        self.lags       = lags = np.arange(16)+1
+        self.lags[0] -= 1
+        self.lags[-1] = 20
+        self.retreats   = np.loadtxt("R_lookuptable.txt").T
+        #Temporary splines
+        self.ins_spline = IUS(ins_times, insolation)
+        self.ret_spline = interp2d(self.ins_times, self.lags, self.retreats)
         pass
 
     def set_model(self, params):
@@ -41,20 +57,33 @@ class Trough(object):
         self.params = params
         return
         
-    def get_insolation(self, t):
-        pass
+    def get_insolation(self, time):
+        return self.ins_spline(time)
 
-    def get_lag(self):
-        pass
-
-    def get_retreat(self):
-        pass
+    def get_retreat(self, lag, time):
+        return self.ret_spline(lag, time)
 
     def get_accumulation(self):
+        #Model dependent
         pass
 
     def get_trajectory(self):
+        #Model dependent
         pass
 
     def lnlikelihood(self):
+        #Model dependent
         pass
+
+if __name__ == "__main__":
+    print("test")
+    tr = Trough([0], 0)
+    tmin, tmax = tr.ins_times[0], tr.ins_times[-1]
+    times = np.linspace(tmin, tmax, 10000)
+
+    try:
+        import matplotlib.pyplot as plt
+        plt.plot(times, tr.get_insolation(times))
+        plt.show()
+    except Exception:
+        print("Install matplotlib to visualize the test.")
