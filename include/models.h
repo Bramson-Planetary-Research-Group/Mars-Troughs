96ac022ffd82d16701be996781ce75c423688099
Tom McClintock <tmcclintock89@gmail.com>
2018-10-28 11:48:21 -0400

    Got rid of all C code that wasn't doing anything.

--- include/models.h
+++
@@ -1,3 +0,0 @@
-void initialize_basic_splines(double*times, int Ntimes, double * insolation, double*lags, int Nlags, double*retreats, int reinit);
-
-void get_insolation(double*times, int Ntimes, double*insolation);
--- setup.py
+++ setup.py
@@ -4,34 +4,6 @@ import setuptools
 from setuptools import setup, Extension
 import subprocess
 
-#Create the symlink.
-try:
-    os.system('ln -s ../include mars_troughs/include')
-except:
-    OSError
-
-#Include GSL flags
-try:
-    cflags = subprocess.check_output(['gsl-config', '--cflags']).split()
-    lflags = subprocess.check_output(['gsl-config', '--libs']).split()
-    cflags = [cflag.decode("utf-8") for cflag in cflags]
-    lflags = [lflag.decode("utf-8") for lflag in lflags]
-except OSError:
-    raise Exception("must have GSL installed and gsl-config working.")
-
-#Specify the sources
-sources = glob.glob(os.path.join('src','*.c'))
-#and the header files.
-headers = glob.glob(os.path.join('include','*.h'))
-
-#Create the extension.
-ext=Extension("mars_troughs._mars_troughs",
-              sources,
-              depends=headers,
-              include_dirs=['include'],
-              extra_compile_args=[os.path.expandvars(flag) for flag in cflags],
-              extra_link_args=[os.path.expandvars(flag) for flag in lflags])
-
 #Create the distribution
 dist = setup(name="mars_troughs",
              author="Tom McClintock",
@@ -41,20 +13,6 @@ dist = setup(name="mars_troughs",
              url="https://github.com/tmcclintock/Mars-Troughs",
              include_package_data = True,
              packages=['mars_troughs'],
-             package_data={'mars_troughs' : headers },
-             ext_modules=[ext],
-             install_requires=['cffi','numpy'],
+             install_requires=['numpy'],
              setup_requires=['pytest_runner'],
              tests_require=['pytest'])
-
-#setup.py doesn't put the .so file in the mars_troughss directory, 
-#so this bit makes it possible to
-#import mars_troughs from the root directory.  
-#Not really advisable, but everyone does it at some
-#point, so might as well facilitate it.
-build_lib = glob.glob(os.path.join('build','*','mars_troughs','_mars_troughs*.so'))
-
-if len(build_lib) >= 1:
-    lib = os.path.join('mars_troughs','_mars_troughs.so')
-    if os.path.lexists(lib): os.unlink(lib)
-    os.link(build_lib[0], lib)
