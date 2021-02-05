8fbde6fc606790ea1650583c8d88c26f4e2b7fcc
Tom McClintock <tmcclintock89@gmail.com>
2021-02-04 19:41:07 -0800

    Better setup

--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
-from setuptools import setup, Extension
+import os
 
-#Create the distribution
-dist = setup(name="mars_troughs",
-             author="Tom McClintock",
-             author_email="tmcclintock89@gmail.com",
-             description="Modules for modeling ice troughs on the Mars North polar ice cap.",
-             license="MIT License",
-             url="https://github.com/tmcclintock/Mars-Troughs",
-             include_package_data = True,
-             packages=['mars_troughs'],
-             install_requires=['numpy'],
-             setup_requires=['pytest_runner'],
-             tests_require=['pytest'])
+PATH_ROOT = os.path.dirname(__file__)
+from setuptools import setup
+
+import mars_troughs  # noqa: E402
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# Create the distribution
+dist = setup(
+    name="mars_troughs",
+    author="Tom McClintock",
+    author_email="tmcclintock89@gmail.com",
+    version=mars_troughs.__version__,
+    description=mars_troughs.__docs__,
+    long_description=long_description,
+    license="MIT License",
+    url="https://github.com/tmcclintock/Mars-Troughs",
+    include_package_data=True,
+    packages=["mars_troughs"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
