c1ea192858e7e098f18db0500cbebfdba8fb6fc7
Tom McClintock <tmcclintock89@gmail.com>
2021-02-04 19:54:53 -0800

    Moved main code to an example file.

--- mars_troughs/trough.py
+++ mars_troughs/trough.py
@@ -1,5 +1,3 @@
-"""This has the trough object with all the necessary functions.
-"""
 import inspect
 import os
 
@@ -179,71 +177,3 @@ class Trough(object):
         xvar, yvar = (self.errorbar * self.meters_per_pixel) ** 2
         chi2 = (xd - xn) ** 2 / xvar + (yd - yn) ** 2 / yvar
         return -0.5 * chi2.sum() - 0.5 * self.Ndata * np.log(xvar * yvar)
-
-
-if __name__ == "__main__":
-    print("Test main() in trough.py")
-    test_acc_params = [1e-8, 5e-9]
-    acc_model_number = 1
-    test_lag_params = [1, 1e-9]
-    lag_model_number = 1
-    errorbar = 100.0
-    tr = Trough(
-        test_acc_params,
-        test_lag_params,
-        acc_model_number,
-        lag_model_number,
-        errorbar,
-    )
-
-    import matplotlib.pyplot as plt
-
-    # plt.rc("text", usetex=True)
-    plt.rc("font", size=14, family="serif")
-    times = tr.ins_times
-    fig, ax = plt.subplots(ncols=2, nrows=3, sharex=True)
-    ax[0, 0].set_ylabel("Ins(t)")
-    ax[1, 0].set_ylabel("-A(t)")
-    ax[2, 0].set_ylabel(r"$y(t)$")  # = \int_0^t{\rm d}t'\ A(t')$")
-    ax[2, 0].set_xlabel(r"Lookback Time (yrs)")
-    ax[2, 1].set_xlabel(r"Lookback Time (yrs)")
-    ax[1, 0].ticklabel_format(
-        style="scientific", scilimits=(0, 0)
-    )  # , useMathText=True)
-    ax[2, 0].ticklabel_format(
-        axis="x", style="sci", scilimits=(0, 0)
-    )  # , useMathText=True)
-    ax[2, 1].ticklabel_format(
-        axis="x", style="sci", scilimits=(0, 0)
-    )  # , useMathText=True)
-    ax[0, 0].plot(times, tr.get_insolation(times))
-    ax[1, 0].plot(times, tr.get_accumulation(times))
-    ax[2, 0].plot(times, tr.get_yt(times))
-
-    ax[0, 1].plot(times, tr.get_lag_at_t(times))
-    ax[1, 1].plot(times, tr.retreat_model_spline(times))
-    ax[2, 1].plot(times, tr.get_xt(times))
-    for i in range(3):
-        ax[i, 1].yaxis.tick_right()
-        ax[i, 1].yaxis.set_label_position("right")
-    ax[0, 1].set_ylabel(r"Lag(t) (mm)")
-    ax[1, 1].set_ylabel(r"$R(t)$ (km)")
-    ax[2, 1].set_ylabel(
-        r"$x(t)$"
-    )  # = \int_0^t{\rm d}t' \frac{R(t') + A(t')\cos(\theta)}{\sin(\theta)}$")
-    ax[2, 1].ticklabel_format(
-        axis="y", style="sci", scilimits=(0, 0)
-    )  # , useMathText=True)
-
-    plt.subplots_adjust(wspace=0.1)
-    fig.savefig("example_plot.png", dpi=300, bbox_inches="tight")
-    # plt.show()
-    plt.clf()
-
-    # Compare the trajectory with data
-    plt.plot(tr.get_xt(times), tr.get_yt(times))
-    plt.errorbar(tr.xdata, tr.ydata, yerr=tr.errorbar, c="k", marker=".", ls="")
-    xn, yn = tr.get_nearest_points()
-    plt.plot(xn, yn, ls="", marker="o", c="r")
-    print(tr.lnlikelihood())
-    plt.show()
