e3d8721767d9ed0aaa0d969ae99861088a1ac39a
Tom McClintock <tmcclintock89@gmail.com>
2021-03-13 09:32:32 -0800

    Docstrings for the Dataset

--- mars_troughs/dataset.py
+++ mars_troughs/dataset.py
@@ -7,6 +7,14 @@ import numpy as np
 
 
 class Dataset:
+    """
+    A class for holding the TMP data.
+
+    Args:
+        filepath (str): path to a data file with two columns, the horizontal
+            and vertical components of the path
+    """
+
     def __init__(self, filepath: str):
         x, y = np.loadtxt(filepath, unpack=True)
         self.x = x
@@ -16,6 +24,15 @@ class Dataset:
     def __getitem__(
         self, index: Union[Iterable, slice]
     ) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Return a tuple of horizontal and vertical positions of the
+        TMP.
+
+        Args:
+            index (Union[Iterable, slice]): indices to select points from
+                the horizontal and vertical components of the path saved
+                in-memory
+        """
         x = self.x[index]
         y = self.y[index]
         return (x, y)
