669dcdce0d7a524d88ec7a3e0420338af451f24b
Tom McClintock <tmcclintock89@gmail.com>
2021-04-11 09:38:56 -0700

    Updated author info to have my up to date email address and added Kris's info.

--- mars_troughs/__init__.py
+++ mars_troughs/__init__.py
@@ -5,6 +5,9 @@ from .lag_model import ConstantLag, LagModel, LinearLag
 from .model import Model
 from .trough import Trough
 
-__author__ = "Tom McClintock <tmcclintock@email.arizona.edu>"
+__author__ = [
+    "Tom McClintock <thmsmcclintock@gmail.com>",
+    "Kristell Izquierdo <kristell.izquierdo@gmail.com>",
+]
 __version__ = "0.0.1"
 __docs__ = "Simulating martian ice troughs."
