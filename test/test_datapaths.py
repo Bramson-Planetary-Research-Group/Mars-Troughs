4d126dd3e1edd9666c4d4078336d06ff274910e4
Tom McClintock <tmcclintock89@gmail.com>
2021-04-16 19:58:35 -0700

    DATAPATHS test

--- test/test_datapaths.py
+++ test/test_datapaths.py
@@ -1,10 +1,12 @@
 import os.path as op
 from unittest import TestCase
 
-from mars_troughs import DEFAULT_DATAPATH_DICT
+from mars_troughs import DATAPATHS
 
 
 class DatapathsTest(TestCase):
     def test_paths_exist(self):
-        for path in DEFAULT_DATAPATH_DICT.values():
-            assert op.exists(path)
+        assert op.exists(DATAPATHS.DATA)
+        assert op.exists(DATAPATHS.INSOLATION)
+        assert op.exists(DATAPATHS.RETREAT)
+        assert op.exists(DATAPATHS.TMP)
