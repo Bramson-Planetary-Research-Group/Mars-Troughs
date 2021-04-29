a0be083f1f09e12ef3df9dbfe2f8148f7fd1d480
kristelizq <kig@umd.edu>
2021-04-29 13:38:21 -0400

    Modified constant and linear lag models.

--- mars_troughs/lag_model.py
+++ mars_troughs/lag_model.py
@@ -2,27 +2,24 @@
 Models for the lag as a function of time.
 """
 from abc import abstractmethod
-from typing import Dict
-
 import numpy as np
 
-from mars_troughs.model import Model
 
-
-class LagModel(Model):
+class LagModel():
     """
-    Abstract class for lag models, that have a method called :meth:`lag`
-    that returns the lag as a function of time.
+    Abstract class for lag models, that have a method 
+    called :meth:`get_lag_at_t` that returns the lag 
+    as a function of time.
     """
 
     @abstractmethod
-    def lag(self, time: np.ndarray) -> np.ndarray:
+    def get_lag_at_t(self, time: np.ndarray):
         raise NotImplementedError
 
 
 class ConstantLag(LagModel):
     """
-    Constant lag model. The lag thickness does not depend on time at all.
+    The lag thickness is constant and does not depend on time.
 
     Args:
         constant (float, optional): default is 1 millimeter. The lag
@@ -32,16 +29,16 @@ class ConstantLag(LagModel):
     def __init__(self, constant: float = 1.0):
         self.constant = constant
 
-    @property
-    def parameters(self) -> Dict[str, float]:
-        return {"constant": self.constant}
-
-    def lag(self, time: np.ndarray) -> np.ndarray:
+    def get_lag_at_t(self, time: np.ndarray):
         """
-        Lag as a function of time. Always returns a constant.
+        Lag as a function of time: returns constant lag values.
 
         Args:
-            time (np.ndarray): ignored
+            time (np.ndarray): times at which we want to calculate the lag.
+        Output:
+            np.ndarray of the same size as time input containing values of lag. 
+            All elements in the array are the same since the lag is
+            constant.
         """
         return self.constant * np.ones_like(time)
 
@@ -61,15 +58,91 @@ class LinearLag(LagModel):
         self.intercept = intercept
         self.slope = slope
 
-    @property
-    def parameters(self) -> Dict[str, float]:
-        return {"intercept": self.intercept, "slope": self.slope}
-
-    def lag(self, time: np.ndarray) -> np.ndarray:
+    def get_lag_at_t(self, time: np.ndarray):
         """
-        Lag as a function of time.
+        Lag as a function of time: lag(t) = a + b*t.
 
         Args:
-            time (np.ndarray): times to evaluate the lag
+            time (np.ndarray): times at which we want to calculate the lag.
+        Output:
+            np.ndarray of the same size as time input containing values of lag. 
+        
         """
         return self.intercept + self.slope * time
+    
+    
+    
+# from abc import abstractmethod
+
+# import numpy as np
+
+# from mars_troughs.model import Model
+    
+
+# class LagModel(Model):
+#     """
+#     Abstract class for lag models, that have a method called :meth:`lag`
+#     that returns the lag as a function of time.
+#     """
+
+#     @abstractmethod
+#     def lag(self, time: np.ndarray) -> np.ndarray:
+#         raise NotImplementedError
+
+
+# class ConstantLag(LagModel):
+#     """
+#     Constant lag model. The lag thickness does not depend on time at all.
+
+#     Args:
+#         constant (float, optional): default is 1 millimeter. The lag
+#             thickness at all times.
+#     """
+
+#     def __init__(self, constant: float = 1.0):
+#         self.constant = constant
+
+#     @property
+#     def parameters(self) -> Dict[str, float]:
+#         return {"constant": self.constant}
+
+#     def lag(self, time: np.ndarray) -> np.ndarray:
+#         """
+#         Lag as a function of time. Always returns a constant.
+
+#         Args:
+#             time (np.ndarray): ignored
+#         """
+#         return self.constant * np.ones_like(time)
+
+
+# class LinearLag(LagModel):
+#     """
+#     The lag thickness is linear in time.
+
+#     Args:
+#         intercept (float, optional): default is 1 millimeter. The lag
+#             thickness at time t=0 (present day).
+#         slope (float, optional): default is 1e-6 mm per year. The rate
+#             of change of the lag each year.
+#     """
+
+#     def __init__(self, intercept: float = 1.0, slope: float = 1e-6):
+#         self.intercept = intercept
+#         self.slope = slope
+
+#     @property
+#     def parameters(self) -> Dict[str, float]:
+#         return {"intercept": self.intercept, "slope": self.slope}
+
+#     def lag(self, time: np.ndarray) -> np.ndarray:
+#         """
+#         Lag as a function of time.
+
+#         Args:
+#             time (np.ndarray): times to evaluate the lag
+#         """
+#         return self.intercept + self.slope * time
+    
+    
+    
