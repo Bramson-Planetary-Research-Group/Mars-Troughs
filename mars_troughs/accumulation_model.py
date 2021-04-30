f6e2b12c5b0beee22ebb207708351da40a454463
kristelizq <kig@umd.edu>
2021-04-30 08:43:25 -0400

    Included second order polynomial for accumulation rate.

--- mars_troughs/accumulation_model.py
+++ mars_troughs/accumulation_model.py
@@ -43,9 +43,9 @@ class LinearAccuIns(AccumulationDepInsolation):
     Args:
         times (np.ndarray): times at which the solar insolation is known
         insolations (np.ndarray): value of the solar insolations
-        intercept (float, optional): default is 0 millimeters
-        slope (float, optional): default is 1e-6 mm per unit
-            of solar insolation square.
+        intercept (float, optional): default is 0 m
+        slope1 (float, optional): default is 1e-6 m per unit
+            of solar insolation.
     """
     def __init__(self, intercept: float = 0.0, slope: float = 1e-6):
         
@@ -60,11 +60,44 @@ class LinearAccuIns(AccumulationDepInsolation):
         Args:
             time (np.ndarray): times at which we want to calculate the lag.
         Output:
+            np.ndarray of the same size as time input containing values of 
+            accumulation. 
+        
+        """
+        return self.intercept -1 * (self.slope * self.ins_data_spline(time))
+        
+class SquareAccuIns(AccumulationDepInsolation):
+    """
+    Accumulation is two degree polynomial of insolaton.
+
+    Args:
+        times (np.ndarray): times at which the solar insolation is known
+        insolations (np.ndarray): value of the solar insolations
+        intercept (float, optional): default is 0 m
+        slope1 (float, optional): default is 1e-6 m per unit
+            of solar insolation.
+        slope2 (float, optional): default is 1e-6 m per unit
+            of solar insolation square.
+    """
+    def __init__(self, intercept: float = 0.0, slope1: float = 1e-6,
+                 slope2: float = 1e-6):
+        
+        self.intercept=intercept
+        self.slope1=slope1
+        self.slope2=slope2
+        
+    def get_accumulation_at_t(self, time: np.ndarray):
+        """
+        Accumulation as a  linear function of insolation: 
+        accu(t) = a + b*ins(t).
+
+        Args:
+            time (np.ndarray): times at which we want to calculate the lag.
+        Output:
             np.ndarray of the same size as time input containing values of lag. 
         
         """
-        return self.intercept + self.slope * time
-        
-        
+        return self.intercept -1 * (self.slope1 * self.ins_data_spline(time) +
+               self.slope2 * self.ins2_data_spline(time))  
 
 
