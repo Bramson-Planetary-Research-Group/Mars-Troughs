e5823050edd49b9c3ab00619090c5332c3f1e57d
klaferr <klafer@terpmail.umd.edu>
2021-05-16 11:48:54 -0400

    Updated to match most up-to-date version on main

--- mars_troughs/__init__.py
+++ mars_troughs/__init__.py
@@ -1,17 +1,17 @@
 from .accumulation_model import (
+    ACCUMULATION_MODEL_MAP,
     AccumulationModel,
     LinearInsolationAccumulation,
     QuadraticInsolationAccumulation,
 )
 from .datapaths import DATAPATHS
-from .lag_model import ConstantLag, LagModel, LinearLag
-from .linear_model import LinearModel, QuadModel
+from .lag_model import ConstantLag, LAG_MODEL_MAP, LagModel, LinearLag
 from .model import Model
 from .trough import Trough
 
 __author__ = [
     "Tom McClintock <thmsmcclintock@gmail.com>",
-    "Kristell Izquierdo <kristell.izquierdo@gmail.com>",
+    "Kristel Izquierdo <kristell.izquierdo@gmail.com>",
 ]
 __version__ = "0.0.1"
 __docs__ = "Simulating martian ice troughs."
