527689c4213706393f19bcbc294d164265505fae
klaferr <klafer@terpmail.umd.edu>
2021-05-26 12:02:10 -0400

    updated test to handle obliquity model

--- test/test_accumulation_model.py
+++ test/test_accumulation_model.py
@@ -6,6 +6,7 @@ from scipy.interpolate import InterpolatedUnivariateSpline as IUS
 from mars_troughs import (
     LinearInsolationAccumulation,
     QuadraticInsolationAccumulation,
+    ObliquityLinearAccumulation,
 )
 
 
@@ -171,6 +172,76 @@ class QuadraticAccumulationTest(TestCase):
         model = QuadraticInsolationAccumulation(
             time, insolation, intercept, linearCoeff, quadCoeff
         )
+        xt = model.get_xt(time, spline, cot_angle, csc_angle)
+        assert (xt != np.nan).all()
+        assert (xt != np.inf).all()
+        assert np.size(xt) == np.size(time)
+
+class ObliquityLinearAccumulationTest(TestCase):
+    def test_smoke(self):
+        obliquity = np.sin(np.radians(np.linspace(0, 360, 100)))
+        times = np.linspace(0, 100, 100)
+        model = ObliquityLinearAccumulation(times, obliquity)
+        assert model is not None
+        assert isinstance(model.intercept, float)
+        assert isinstance(model.slope, float)
+
+    def test_parameter_names(self):
+        obliquity = np.sin(np.radians(np.linspace(0, 360, 100)))
+        times = np.linspace(0, 100, 100)
+        model = ObliquityLinearAccumulation(times, obliquity)
+        assert model.parameter_names == ["intercept", "slope"]
+
+    def test_constant(self):
+        obliquity = np.sin(np.radians(np.linspace(0, 360, 100)))
+        times = np.linspace(0, 100, 100)
+        model1 = ObliquityLinearAccumulation(times, obliquity)
+        model2 = ObliquityLinearAccumulation(times, obliquity, 2.0, 1e-7)
+        assert model1.intercept != model2.intercept
+        assert model1.slope != model2.slope
+        assert set(model1.parameters.keys()) == set(model2.parameters.keys())
+        assert model1.parameters != model2.parameters
+
+    def test_lag(self):
+        time = np.linspace(0, 1e6, 1000)
+        obliquity = np.sin(np.radians(np.linspace(0, 360, 1000)))
+        for inter, slope in zip([1.0, 2.0, 3.0], [1e-6, 2e-6, 3e-5]):
+            model = ObliquityLinearAccumulation(time, obliquity, inter, slope)
+            assert model.intercept == inter
+            assert model.slope == slope
+            accums = model.get_accumulation_at_t(time)
+            assert (accums == inter + slope * model._ins_data_spline(time)).all()
+
+    def test_get_yt(self):
+        time = np.linspace(0, 1e6, 1000)
+        obliquity = np.sin(np.radians(np.linspace(0, 360, 1000)))
+        for inter, slope in zip([1.0, 2.0, 3.0], [1e-6, 2e-6, 3e-5]):
+            model = ObliquityLinearAccumulation(time, obliquity, inter, slope)
+            yt = model.get_yt(time)
+            assert (
+                yt
+                == -(
+                    inter * time
+                    + (
+                        slope
+                        * (
+                            model._int_ins_data_spline(time)
+                            - model._int_ins_data_spline(0)
+                        )
+                    )
+                )
+            ).all()
+
+    def test_get_xt(self):
+        time = np.linspace(0, 1e6, 1000)
+        obliquity = np.sin(np.radians(np.linspace(0, 360, 1000)))
+        spline = np.linspace(0, 100, 1000)
+        spline = IUS(time, obliquity)
+        csc_angle = np.radians(np.linspace(0, 360, 1000))
+        cot_angle = np.radians(np.linspace(0, 360, 1000))
+        intercept = 1.0
+        slope = 1e-6
+        model = ObliquityLinearAccumulation(time, obliquity, intercept, slope)
         xt = model.get_xt(time, spline, cot_angle, csc_angle)
         assert (xt != np.nan).all()
         assert (xt != np.inf).all()
