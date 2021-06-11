2f568e6e4ca5d591bf323b4217a6980b26ceb181
Tom McClintock <tmcclintock89@gmail.com>
2021-06-10 19:10:04 -0700

    Fixed a github screwup.

--- mars_troughs/accumulation_model.py
+++ mars_troughs/accumulation_model.py
@@ -102,7 +102,7 @@ class Linear_Insolation(TimeDependentAccumulationModel, LinearModel):
     def __init__(
         self,
         times: np.ndarray,
-        insolations: np.ndarray, 
+        insolations: np.ndarray,
         intercept: float = 1e-6,
         slope: float = 1e-6,
     ):
@@ -222,45 +222,8 @@ class Linear_Obliquity(TimeDependentAccumulationModel, LinearModel):
                 self.slope
                 * (self._int_var_data_spline(time) - self._int_var_data_spline(0))
             )
-            )
-    
-    
-    
-class Linear_Obliquity(TimeDependentAccumulationModel, LinearModel):
-    
-    def __init__(self, 
-        obl_times: np.ndarray, 
-        obliquity: np.ndarray, 
-        intercept: float=1.0, 
-        slope: float=1.0):
-        
-        LinearModel.__init__(self, intercept, slope)
-        super().__init__(obl_times, obliquity)
-    
-    def get_yt(self, time: np.ndarray):
-        """
-        Calculates the vertical distance y (in m) traveled by a point
-        in the center of the high side of the trough. This distance  is a
-        function of the accumulation rate A as y(t)=integral(A(obl(t)), dt) or
-        dy/dt=A(obl(t))
-
-        Args:
-            time (np.ndarray): times at which we want to calculate y, in years.
-        Output:
-            np.ndarray of the same size as time input containing values of
-            the vertical distance y, in meters.
-
-        """
-
-        return -(
-            self.intercept * time
-            + (
-                self.slope
-                * (self._int_var_data_spline(time) - self._int_var_data_spline(0))
-            )
         )
 
-    
 
 ACCUMULATION_MODEL_MAP: Dict[str, Model] = {
     "linear": Linear_Insolation,
