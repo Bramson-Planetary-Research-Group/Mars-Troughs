edde23f0c4c8e0e074cdbc72fd8953d89ce65154
Tom McClintock <tmcclintock89@gmail.com>
2021-08-22 21:37:11 -0700

    New names in the lag area

--- test/lag_model_test.py
+++ test/lag_model_test.py
@@ -38,17 +38,17 @@ class LinearLagTest(TestCase):
     def test_smoke(self):
         model = LinearLag()
         assert model is not None
-        assert isinstance(model.intercept, float)
+        assert isinstance(model.constant, float)
         assert isinstance(model.slope, float)
 
     def test_parameter_names(self):
         model = LinearLag()
-        assert model.parameter_names == ["intercept", "slope"]
+        assert model.parameter_names == ["constant", "slope"]
 
     def test_constant(self):
         model1 = LinearLag()
         model2 = LinearLag(2.0, 1e-7)
-        assert model1.intercept != model2.intercept
+        assert model1.constant != model2.constant
         assert model1.slope != model2.slope
         assert set(model1.parameters.keys()) == set(model2.parameters.keys())
         assert model1.parameters != model2.parameters
@@ -57,7 +57,7 @@ class LinearLagTest(TestCase):
         time = np.linspace(0, 1e6, 1000)
         for inter, slope in zip([1.0, 2.0, 3.0], [1e-6, 2e-6, 3e-5]):
             model = LinearLag(inter, slope)
-            assert model.intercept == inter
+            assert model.constant == inter
             assert model.slope == slope
             lags = model.get_lag_at_t(time)
             assert (lags == inter + slope * time).all()
